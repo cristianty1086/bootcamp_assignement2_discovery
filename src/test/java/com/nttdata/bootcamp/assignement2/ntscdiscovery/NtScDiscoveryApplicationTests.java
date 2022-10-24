f49ea7cc08cc150eb0af6cbd03972cd7da3248f5
cristianty1086 <ctinocoy123@gmail.com>
2022-10-24 15:29:00 -0500

    first commit

---
+++ src/test/java/com/nttdata/bootcamp/assignement2/ntscdiscovery/NtScDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.assignement2.ntscdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtScDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
